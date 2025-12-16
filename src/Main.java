9bbc10f42d6b85035586b176a9066c8c46df12b6
Nurislam <taziddinovnurislam@gmail.com>
2025-12-17 00:01:54 +0500

    .

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+
+
 public class Main {
     public static void main(String[] args) {
-    Car car1 = new Car();
-    car1.setColor("white");
+        Car car1 = new Car();
+        car1.setColor("white");
         System.out.println(car1.getColor());
+        System.out.println("Hello");
     }
+
 }
